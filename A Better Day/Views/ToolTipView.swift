9c62d1a2ea581ad68fab56243d710d637e7df621
Jack Ferris <JackDFerris03@gmail.com>
2025-05-29 19:06:57 -0400

    Updated today page with image, and created enum for switching pages on buttons outside of navbar

---
+++ A Better Day/Views/ToolTipView.swift
@@ -0,0 +1,18 @@
+//
+//  ToolTipView.swift
+//  A Better Day
+//
+//  Created by Jack Ferris on 5/29/25.
+//
+
+import SwiftUI
+
+struct ToolTipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToolTipView()
+}
